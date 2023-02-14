8e41acb83d77d9874f2ab4dff5f20fc7560508fb
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-02-14 13:16:05 -0300

    Feat: installation aos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import { ThemeProvider } from 'styled-components';
+import { useEffect } from 'react';
+import Aos from 'aos';
 
 import GlobalStyles from '../styles/global';
 import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    Aos.init({ duration: 1500 });
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
