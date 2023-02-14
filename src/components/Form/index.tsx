64decba7ce02eca181c01aa46ef9a2b374f918dc
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-02-14 17:47:15 -0300

    Feat: init add form response

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,10 +1,42 @@
+import { FormEvent, useState } from 'react';
+import { sendContactMail } from '../../services/sandMail';
 import { Container, Input, TextArea } from './styles';
 
-export const Form = () => (
-  <Container data-aos="fade-up">
-    <Input placeholder="Nome" type="name" required />
-    <Input placeholder="E-mail" type="email" required />
-    <TextArea placeholder="Mensagem" required as="textarea" />
-    <button type="submit">Enviar</button>
-  </Container>
-);
+export const Form = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [massage, setMassage] = useState('');
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    await sendContactMail(name, email, massage);
+  };
+
+  return (
+    <Container data-aos="fade-up" onSubmit={handleSubmit}>
+      <Input
+        onChange={({ target }) => setName(target.value)}
+        placeholder="Nome"
+        value={name}
+        type="name"
+        required
+      />
+      <Input
+        placeholder="E-mail"
+        onChange={({ target }) => setEmail(target.value)}
+        value={email}
+        type="email"
+        required
+      />
+      <TextArea
+        onChange={({ target }) => setMassage(target.value)}
+        placeholder="Mensagem"
+        value={massage}
+        as="textarea"
+        required
+      />
+      <button type="submit">Enviar</button>
+    </Container>
+  );
+};
---
+++ src/pages/api/contact.ts
@@ -0,0 +1,40 @@
+import nodemailer from 'nodemailer';
+import sendgridTransport from 'nodemailer-sendgrid-transport';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const email = process.env.MAIL_ADRESS;
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key: process.env.SENDGRID_API_KEY
+    }
+  })
+);
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { senderMail, name, content } = req.body;
+
+    if (!senderMail.trim() || !name.trim() || !content.trim()) {
+      return res.status(403).send('');
+    }
+
+    const message = {
+      from: email,
+      to: email,
+      subject: `Nova mensagem de contato - ${name}`,
+      html: `<p><b>Email:</b> ${senderMail}<br /><b>Mensagem:</b> ${content}</p>`,
+      replyTo: senderMail
+    };
+
+    await transporter.sendMail(message);
+
+    return res.send('ok');
+  } catch (err) {
+    return res.json({
+      error: true,
+      message: err.message
+    });
+  }
+};
