def4d33ec534cf889873ae8e940b1983068098e3
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-03-01 21:40:33 -0300

    Fix: Link and button tag order change to optimize button functionality and insert ID

--- src/components/Courses/styles.ts
+++ src/components/Courses/styles.ts
@@ -5,8 +5,8 @@ export const Container = styled.section`
   display: flex;
   width: 100%;
   flex-direction: column;
-  align-items: center;
   gap: 5rem;
+  align-items: center;
 
   > section {
     display: flex;
@@ -19,26 +19,29 @@ export const Container = styled.section`
     }
   }
 
-  > button {
-    background: ${({ theme }) => theme.primary};
-    padding: 0.8rem 3rem;
-    border-radius: 0.5rem;
-    border: none;
-    transition: 0.5s;
+  a:first-child {
+    margin: 0 auto 0 0;
+    cursor: default;
+  }
 
-    &:hover {
-      background: ${({ theme }) => darken(0.05, theme.primary)};
-    }
+  a:not(:first-child) {
+    color: ${({ theme }) => theme.background};
+    text-transform: uppercase;
 
-    a {
-      color: ${({ theme }) => theme.background};
-      text-transform: uppercase;
+    > button {
+      background: ${({ theme }) => theme.primary};
+      padding: 0.8rem 3rem;
+      border-radius: 0.5rem;
+      border: none;
+      transition: 0.5s;
       font-size: 1.5rem;
-    }
 
-    @media (max-width: 500px) {
-      padding: 1rem;
-      a {
+      &:hover {
+        background: ${({ theme }) => darken(0.05, theme.primary)};
+      }
+
+      @media (max-width: 500px) {
+        padding: 1rem;
         font-size: 1rem;
       }
     }
