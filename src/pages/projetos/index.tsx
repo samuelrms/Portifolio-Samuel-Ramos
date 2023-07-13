36a3206de6b9c3a0ee561d79f768675df08f9911
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-07-13 12:21:37 -0300

    feat: SEO optimization of the project page

--- src/pages/projetos/index.tsx
+++ src/pages/projetos/index.tsx
@@ -10,19 +10,35 @@ export default function Projetos({ projects }: PropsProjectArr) {
   return (
     <Container>
       <Head>
-        <title>Projetos | Portifólio Samuel Ramos</title>
+        <title>Projetos | Portifólio Samuel Ramos - Full-Stack Developer</title>
+        <meta charSet="utf-8" />
+        <meta name="language" content="pt-BR" />
         <meta
           name="description"
           content="Sinta-se à vontade para explorar meus projetos e entrar em contato comigo se tiver alguma dúvida ou quiser saber mais sobre meu trabalho. Obrigado por visitar!"
         />
-        <meta property="og:image" content="/ogimage.png" />
-        <meta property="og:image:secure_url" content="/ogimage.png" />
-        <meta property="instagram:image" content="/ogimage.png" />
-        <meta property="instagram:image:src" content="/ogimage.png" />
+        <meta name="robots" content="all" />
+        <meta name="author" content="Samuel Ramos" />
+        <meta
+          name="keywords"
+          content="nodejs-javascript-typescript-react-next-nestjs-wordpress-freelancer"
+        />
+
+        <meta property="og:type" content="page" />
+        <meta property="og:url" content="samuelramos.dev/projetos" />
+        <meta
+          property="og:title"
+          content="Projetos | Portifólio Samuel Ramos - Full-Stack Developer"
+        />
+        <meta
+          property="og:image"
+          content="https://images.prismic.io/portifolio-samuel-rms/722b4aac-b5a4-4924-9c8a-61bf9096a108_projetos.png?auto=compress,format"
+        />
         <meta
           property="og:description"
           content="Sinta-se à vontade para explorar meus projetos e entrar em contato comigo se tiver alguma dúvida ou quiser saber mais sobre meu trabalho. Obrigado por visitar!"
         />
+        <meta property="article:author" content="Samuel Ramos" />
       </Head>
       <main className="container">
         <SectionTitle title="Projetos" />
