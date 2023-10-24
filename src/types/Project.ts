6788dd6146c092d5aeb2f5e69fd801e5e1ef91bf
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-10-24 04:02:07 -0300

    feat: Start changing the site's projects, using the GitHub API

---
+++ src/types/Project.ts
@@ -0,0 +1,18 @@
+export type ReadmeContent = {
+  name: string;
+  path: string;
+  sha: string;
+  size: number;
+  url: string;
+  html_url: string;
+  git_url: string;
+  download_url: string;
+  type: string;
+  content: string;
+  encoding: string;
+  _links: {
+    self: string;
+    git: string;
+    html: string;
+  };
+};
